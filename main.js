c9c22bfcf49498608379b74c408659bce59af627
KurtHub-dev <111973966+KurtHub-dev@users.noreply.github.com>
2022-12-07 17:13:59 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Car counter
 let count = 0
 let counting = document.getElementById("counter")
 let saveP = document.getElementById("savePast")
@@ -11,5 +12,75 @@ function save() {
     let saveNew = count + " - "
     saveP.textContent += saveNew
     counting.textContent = 0
-    return count = 0
+}
+
+//Calculator
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let sum = document.getElementById("sum-el")
+let answer = 0
+
+function add() {
+    answer = num1 + num2
+    sum.textContent = "Sum: " + answer
+}
+function subtract() {
+    answer = num1 - num2
+    sum.textContent = "Sum: " + answer
+}
+
+function divide() {
+    answer = num1 / num2
+    sum.textContent = "Sum: " + answer
+}
+
+function multiply() {
+    answer = num1 * num2
+    sum.textContent = "Sum: " + answer
+}
+
+//Score Board
+
+let scoreHome = document.getElementById("score-home")
+let scoreGuest = document.getElementById("score-guest")
+let totalScoreh = 0
+let totalScoreg = 0
+
+function plush1() {
+    totalScoreh += 1
+    scoreHome.textContent = totalScoreh
+}
+
+function plush2() {
+    totalScoreh += 2
+    scoreHome.textContent = totalScoreh
+}
+
+function plush3() {
+    totalScoreh += 3
+    scoreHome.textContent = totalScoreh
+}
+
+function plusg1() {
+    totalScoreg += 1
+    scoreGuest.textContent = totalScoreg
+}
+
+function plusg2() {
+    totalScoreg += 2
+    scoreGuest.textContent = totalScoreg
+}
+
+function plusg3() {
+    totalScoreg += 3
+    scoreGuest.textContent = totalScoreg
+}
+
+function newgame() {
+    totalScoreh = 0
+    totalScoreg = 0
+    scoreHome.textContent = totalScoreh
+    scoreGuest.textContent = totalScoreg
 }
